241b038dfd20989208e3cf3a4d4fa579e8a8df08
zhzhn2002 <zhzhn2002@126.com>
2023-03-10 22:03:29 +0800

    变更文件名，更新bonjean函数算法

--- 作业1-20230306/ex1_2021282215.py
+++ 作业1-20230306/ex1_2021282215.py
@@ -152,82 +152,17 @@ def cumul_list(l: list[float]) -> list[float]:
 # print(cumul_list([1, 2, 3, 4, 5]))
 
 
-def bonjean(data: list[tuple]) -> tuple[
-    Optional[list[tuple[float, float]]], Optional[list[tuple[float, float]]], Optional[list[tuple[float, float]]]]:
-    """
-    Calculates the bonjean area and the moments of every section of waterlines.
-
-    Parameters
-    --------
-    data : list[tuple]
-        Contains some points on the bonjean curve.
-
-    Examples
-    --------
-    >>> a, h, v = bonjean([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)])
-    >>> print(a)
-        [(0, 2), (1.5, 3), (4.0, 4), (7.5, 5), (12.0, 6)]
-    >>> print(h)
-        [(0, 2), (1.3333333333333335, 3), (4.666666666666666, 4), (11.5625, 5), (23.59375, 6)]
-    >>> print(v)
-        [(0, 2), (3.666666666666667, 3), (12.333333333333332, 4), (29.21875, 5), (57.453125, 6)]
-
-    Returns
-    --------
-    result: tuple[Optional[list[tuple[float, float]]], Optional[list[tuple[float, float]]], Optional[list[tuple[float, float]]]]
-        A tuple of result, with the 1st element of area, 2nd element of horizontal 1st moment, and 3rd element of vertical 1st moment
-    """
-
-    def combine(data: list[float], waterline: list[float]) -> Optional[list[tuple[float, float]]]:
-        if len(data) != len(waterline):
-            return None
-        else:
-            result = list()
-            for index in range(len(data)):
-                t = (data[index], waterline[index])
-                result.append(t)
-            return result
-
-    def getCentroidOfCombinedShape(centroid1: tuple, area1: float, centroid2: tuple, area2: float):
-        x1 = centroid1[0]
-        x2 = centroid2[0]
-        y1 = centroid1[1]
-        y2 = centroid2[1]
-        x = (x1 * area1 + x2 * area2) / (area1 + area2)
-        y = (y1 * area1 + y2 * area2) / (area1 + area2)
-        return x, y
-
-    waterline = list()
-
-    # 计算面积
-    a0 = [0]
-    for i in range(len(data)):
-        waterline.append(data[i][1])
-    for i in range(len(data) - 1):
-        a0.append(trapezium_params(data[i], data[i + 1])[0])
-    a = combine(cumul_list(a0), waterline)
-
-    # 计算一阶面积矩
-    centroids = []
-    for i in range(len(data) - 1):
-        centroids.append((trapezium_params(data[i], data[i + 1])[1], trapezium_params(data[i], data[i + 1])[2]))
-    for i in range(len(centroids) - 1):
-        x, y = getCentroidOfCombinedShape(
-            (centroids[i][0], centroids[i][1]),
-            a0[i + 1],
-            (centroids[i + 1][0], centroids[i + 1][1]),
-            a0[i + 2]
-        )
-        centroids[i + 1] = (x, y)
-    horizontalMoments = [0]
-    verticalMoments = [0]
-    for i in range(len(data) - 1):
-        horizontalMoments.append(centroids[i][0] * a[i + 1][0])
-        verticalMoments.append(centroids[i][1] * a[i + 1][0])
-    horizontalMoments = combine(horizontalMoments, waterline)
-    verticalMoments = combine(verticalMoments, waterline)
-
-    return a, horizontalMoments, verticalMoments
+def bonjean(data: list[tuple]):
+    def combine(a: list[float], b: list[float]) -> Optional[list[tuple[float, float]]]:
+        return [(a[x], b[x]) for x in range(len(data))] if len(a) == len(b) else None
+    waterline = [(data[i][1]) for i in range(len(data))]
+    a = cumul_list([(trapezium_params(data[i], data[i + 1])[0]) for i in range(len(data) - 1)])
+    a.insert(0, 0)
+    h = cumul_list([(trapezium_params(data[i], data[i + 1])[1] * trapezium_params(data[i], data[i + 1])[0]) for i in range(len(data) - 1)])
+    h.insert(0, 0)
+    v = cumul_list([(trapezium_params(data[i], data[i + 1])[2] * trapezium_params(data[i], data[i + 1])[0]) for i in range(len(data) - 1)])
+    v.insert(0, 0)
+    return combine(a, waterline), combine(h, waterline) ,combine(v, waterline)
 
 
 # a, h, v = bonjean([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)])
